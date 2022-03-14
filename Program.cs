09bdfc198d3b32327ff4c486c18baa3b9a39d2ee
pVang8 <pvang@codestack.co>
2022-03-14 15:31:53 -0700

    added services for dependent

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Scrubby_webApi.Services;
+using Scrubby_webApi.Services.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<DependentService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
