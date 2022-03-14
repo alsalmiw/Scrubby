0af2e5fa56f70fa241e6b5e7a9c298f5d16d6e54
pVang8 <pvang@codestack.co>
2022-03-14 14:03:01 -0700

    dependant controller

---
+++ Controllers/DependantController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scrubby_webApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DependantController : ControllerBase
+    {
+        
+    }
+}
