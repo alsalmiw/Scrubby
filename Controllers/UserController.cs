bc5888e965ad6304109c039f3e72d7cbd93ea29f
alsalmiw <alsalmiw@gmail.com>
2022-03-14 14:03:51 -0700

    created files for user

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace scrubby_webapi.Models
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        
+    }
+}
